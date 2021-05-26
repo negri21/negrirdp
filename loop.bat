51ac37e517b6ce0dabed178b30ae64772297af53
Avishkar Patil <avishkarpatil29@gmail.com>
2021-05-27 00:38:04 +0530

    Initial Commit

--- loop.bat
+++ loop.bat
@@ -1,8 +1,8 @@
 @echo off
-echo AdityaRDP By AdityaGANS!
+echo RDP by Avishkar Patil
 tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > nul
 cls
-echo AdityaRDP By AdityaGANS!
+echo RDP by Avishkar Patil
 goto check
